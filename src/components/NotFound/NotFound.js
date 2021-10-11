7de84195ea82c8d7343ee2c7b27c1e502cc43da9
cuongvu164 <cuongmk288@gmail.com>
2021-10-11 23:43:05 +0700

    Create page 404

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const NotFound = () => {
+  return (
+    <>
+      <div id="content" class="site-content page-404">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 page-left">
+              <div className="image">
+                <img className="img-responsive" src="img/404-left.png" alt="Image_404" />
+              </div>
+              <div className="title">We’re sorry — something has gone wrong on our end.</div>
+              <div className="content">If difficulties persist, please contact the System Administrator of this site and report the error below.</div>
+              <Link className="btn btn-primary" to="/"><i className="fa fa-home" aria-hidden="true" /><span>Back to homepage</span></Link>
+            </div>
+            <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 page-right">
+              <div className="image">
+                <img className="img-responsive" src="img/404-right.jpg" alt="Image_404" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </>
+  );
+};
+
+export default NotFound;
--- src/routers.js
+++ src/routers.js
@@ -3,9 +3,11 @@ import { lazy } from 'react'
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 const ProductPage = lazy(() => import('./pages/ProductPage/ProductPage'));
 const CartPage = lazy(() => import('./pages/CartPage/CartPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
 
 export const routers = [
   { path: '/', exact: true, Component: HomePage },
   { path: '/product', exact: true, Component: ProductPage },
   { path: '/cart', exact: true, Component: CartPage },
+  { path: '*', exact: true, Component: NotFoundPage },
 ]
