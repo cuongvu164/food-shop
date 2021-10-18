48017f1dc88b0dd31214ba3e123796c275933719
cuongvu164 <cuongmk288@gmail.com>
2021-10-18 14:34:33 +0700

    Remove menu dropdown

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,30 +13,6 @@ const Navbar = () => {
           </li>
           <li className="dropdown">
             <Link to="/product" title="Product">Product</Link>
-            <div className="dropdown-menu">
-              <ul>
-                <li className="has-image">
-                  <img src="img/product/product-category-1.png" alt="Product_Category_Image"/>
-                  <Link to="/" title="Vegetables">Vegetables</Link>
-                </li>
-                <li className="has-image">
-                  <img src="img/product/product-category-2.png" alt="Product_Category_Image" />
-                  <Link to="/" title="Fruits">Fruits</Link>
-                </li>
-                <li className="has-image">
-                  <img src="img/product/product-category-3.png" alt="Product_Category_Image" />
-                  <Link to="/" title="Bread">Bread</Link>
-                </li>
-                <li className="has-image">
-                  <img src="img/product/product-category-4.png" alt="Product_Category_Image" />
-                  <Link to="/" title="Juices">Juices</Link>
-                </li>
-                <li className="has-image">
-                  <img src="img/product/product-category-5.png" alt="Product_Category_Image" />
-                  <Link to="/" title="Tea and coffee">Tea and coffee</Link>
-                </li>
-              </ul>
-            </div>
           </li>
           <li className="dropdown">
             <Link to="/blog">Blog</Link>
