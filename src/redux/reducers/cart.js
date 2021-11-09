b492a0d670785e27cb9a4f54530a97e1cbed44d9
cuongvu164 <cuongmk288@gmail.com>
2021-11-09 21:45:04 +0700

    Sort product

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -25,15 +25,15 @@ const findIndex = (cartFood, product) => {
 
 const cartReducer = (state = initialState, action) => {
   const { listProduct, quantity } = action
-  console.log('action cart', action)
+  // console.log('action cart', action)
 
   switch (action.type) {
     case ADD_TO_CART:
-      console.log('action add_to cart-----', state)
-      console.log('action add_to cart2-----', listProduct)
+      // console.log('action add_to cart-----', state)
+      // console.log('action add_to cart2-----', listProduct)
 
       var index = findIndex(state, listProduct)
-      console.log('index add to cart-----', index)
+      // console.log('index add to cart-----', index)
 
       if (index !== -1) {
         state[index].quantity += action.quantity
@@ -63,11 +63,11 @@ const cartReducer = (state = initialState, action) => {
       return [...state]
 
     case DELETE_TO_CART:
-      console.log('action delete_to cart update-----', state)
-      console.log('action delete_to cart update3-----', listProduct)
+      // console.log('action delete_to cart update-----', state)
+      // console.log('action delete_to cart update3-----', listProduct)
 
       index = findIndex(state, listProduct)
-      console.log('index deleteToCart', index)
+      // console.log('index deleteToCart', index)
       if (index !== -1) {
         state.splice(index, 1);
       }
